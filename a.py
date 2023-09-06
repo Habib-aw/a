62a9df400c52552e2b924afaaf62569de2f7d5ec
Habib-aw <88142412+Habib-aw@users.noreply.github.com>
2023-09-06 17:24:12 +0100

    born

---
+++ a.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkvideo import tkvideo
+import pygame
+import os
+
+
+
+def get_audio(file):
+    from moviepy.editor import VideoFileClip
+
+    # Load the MP4 file
+    video = VideoFileClip(file + ".mp4")
+
+    # Extract the audio
+    audio = video.audio
+
+    # Save the audio as an MP3 file
+    audio.write_audiofile(file + ".mp3")
+
+
+def start(file):
+    if not file + ".mp3" in os.listdir("."):
+        get_audio(file)
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(file + ".mp3")
+    root = tk.Tk()
+    # root.geometry("640x480")
+
+    videoPlayer = tk.Label(root)
+    videoPlayer.pack()
+    video = tkvideo(file + ".mp4", videoPlayer, loop=1, size=(root.winfo_screenwidth(),root.winfo_screenheight()))
+    video.play()
+    pygame.mixer.music.play()
+    root.attributes('-fullscreen',True)
+    root.mainloop()
+
+
+start("a")
